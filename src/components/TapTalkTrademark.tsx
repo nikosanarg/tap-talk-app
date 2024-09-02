475837c0e45a50d1ba197e4e738898d3db279509
Nico Sandobal <nikosanarg@gmail.com>
2024-09-02 01:07:34 -0300

    Primer component y config general

---
+++ src/components/TapTalkTrademark.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components/native';
+
+const TrademargText = styled.Text`
+  font-family: "sans-serif";
+  font-size: 24px;
+`
+
+const TapTalkTrademark = () => {
+  return (
+    <TrademargText>
+      Tap-Talk
+    </TrademargText>
+  )
+}
+
+export default TapTalkTrademark
